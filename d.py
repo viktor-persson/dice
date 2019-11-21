4d516851c221eb9e27b951316f682ecf91d0721e
viktor persson <viktor.persson.1@skola.botkyrka.se>
2019-11-21 11:14:36 +0100

    time sleep

--- d.py
+++ d.py
@@ -1,13 +1,16 @@
 import random
+import time
 print("Välkommen till tärningssimulatorn")
 
-ans = input("vill du slå en tärning ")
+ans = input("Vill du slå en tärning ")
 result = random.randint(1, 6)
 while ans == "ja":
     result = random.randint(1, 6)
+    time.sleep(2)
     print(result)
-    ans = input("vill du slå en tärning ")
+    ans = input("Vill du slå en tärning ")
 else:
-     print("ok")
+    time.sleep(2)
+    print("Ok")
 
 
