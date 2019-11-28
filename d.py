ac6aa47d18968b71425774d62bcfafa5d561589f
viktor persson <viktor.persson.1@skola.botkyrka.se>
2019-11-28 11:37:02 +0100

    now you can see the average of the list you create with the dice

--- d.py
+++ d.py
@@ -1,16 +1,24 @@
 import random
 import time
+
+dice_result = []
+
 print("Välkommen till tärningssimulatorn")
+
+def average(numbers):
+    return sum(numbers)/len(numbers)
 
 ans = input("Vill du slå en tärning ")
 result = random.randint(1, 6)
 while ans == "ja":
     result = random.randint(1, 6)
-    time.sleep(2)
+    time.sleep(0.1)
     print(result)
+    dice_result.append(result)
     ans = input("Vill du slå en tärning ")
 else:
-    time.sleep(2)
-    print("Ok")
+    time.sleep(0.1)
+    print(dice_result)
+    print(average(dice_result))
 
 
